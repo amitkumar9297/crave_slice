751c28e2f8f2789fe585c17b25b4438a44e3f3e0
Amit Kumar <amitkumar790194@gmail.com>
2024-08-26 23:42:26 +0530

    login page ui done

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,7 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className=" text-white-100 bg-gradient-to-r from-indigo-700 via-violet-700 to-orange-700 body-font">
+        <div className=" container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
+          <Link
+            href="/"
+            className="flex title-font font-extrabold items-center uppercase text-gray-100"
+          >
+            <Image alt="Logo" src={"/logo.png"} width={"40"} height={"40"} />
+            <p>CraveSlice🫠</p>
+          </Link>
+          <p className=" test-sm text-gray-100 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4 ">
+            Copyright © 2024 Crave Slice
+          </p>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
---
+++ src/pages/login/index.js
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import React, { useState } from "react";
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const handleChange = (e) => {
+    setCredentials({ ...credentials, [e?.target?.name]: e?.target?.value });
+    console.log(credentials);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div
+      style={{
+        height: "90vh",
+        backgroundImage:
+          'url("https://images.pexels.com/photos/326278/pexels-photo-326278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
+        backgroundSize: "cover",
+      }}
+      className="flex justify-center items-center"
+    >
+      <div className="container w-full max-w-md">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-gray-100 dark:bg-gray-900 dark:text-gray-100 border-gradient rounded-lg shadow-2xl px-8 pt-6 pb-8 mb-4"
+        >
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className=" block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
+            >
+              Username
+            </label>
+            <input
+              name="email"
+              type="text"
+              value={credentials?.email}
+              placeholder="Enter your email"
+              onChange={handleChange}
+              className="shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 focus:border-indigo-700 text-gray-700 dark:text-gray-100  leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className=" block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
+            >
+              Password
+            </label>
+            <input
+              name="password"
+              type="password"
+              value={credentials?.password}
+              placeholder="Enter your Password"
+              onChange={handleChange}
+              className="shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 focus:border-indigo-700 text-gray-700 dark:text-gray-100  leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <button
+            type="submit"
+            className="border text-gray-900 dark:text-gray-100 font-bold dark:border-gray-400 border-gray-900 rounded mr-2 p-2 hover:bg-gradient-to-r from-indigo-700 via-violet-700 to-orange-700  hover:text-gray-100"
+          >
+            Log in
+          </button>
+          <Link href={"/signup"} style={{ all: "unset" }}>
+            <button className="border text-gray-900 dark:text-gray-100 font-bold dark:border-gray-400 border-gray-900 rounded mr-2 p-2 hover:bg-gradient-to-r from-indigo-700 via-violet-700 to-orange-700  hover:text-gray-100">
+              New User?
+            </button>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
